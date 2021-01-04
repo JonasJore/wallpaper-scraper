a5509d2019ab9a75427b6e754823662c0ac8aa34
JonasJore <jonas.jore@gmail.com>
2021-01-05 00:12:46 +0100

    now the program downloads broken files... now what

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{ fs::File };
-use std::io;
+use std::fs::File;
+use std::io::copy;
 
 mod types;
 
@@ -11,10 +11,19 @@ fn is_valid_wallpaper_link(s: &str) -> bool {
     return false;
 }
 
+async fn download_link(url: &str, file_name: &str, dir_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    if dir_name.ends_with("/") || dir_name.ends_with("\\") {
+        dir_name.to_string().pop();
+    }
+    let mut resp = reqwest::get(url).await?;
+    let mut out = File::create(format!("{}/{}", dir_name, file_name)).expect("failed to create file");
+    copy(&mut resp.text().await?.as_bytes(), &mut out).expect("failed to copy content");
+    Ok(())
+}
+
 #[tokio::main]
 async fn fetch_from_reddit() -> Result<(), Box<dyn std::error::Error>> {
-    let res = reqwest::get("https://www.reddit.com/r/wallpaper/top.json?t=all&limit=100")
-        .await?;
+    let res = reqwest::get("https://www.reddit.com/r/wallpaper/top.json?t=all&limit=100").await?;
 
     println!("Status: {}", res.status());
 
@@ -24,12 +33,21 @@ async fn fetch_from_reddit() -> Result<(), Box<dyn std::error::Error>> {
 
     for wallpaper in value.data.children {
         if is_valid_wallpaper_link(&wallpaper.data.url) {
-            println!("link: {}", wallpaper.data.url);
+            println!("{}", wallpaper.data.url);
             link_number += 1;
 
-            let mut resp = reqwest::get(&wallpaper.data.url);
-            let mut out = File::create(format!("{}/{}", "dir_name", &link_number.to_string())).expect("failed to create file");
-            io::copy(&mut resp, &mut out).expect("failed to copy content");
+            let response = reqwest::get(&wallpaper.data.url).await?;
+
+            let file_name = response
+                .url()
+                .path_segments()
+                .and_then(|segments| segments.last())
+                .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                .unwrap_or("tmp.bin");
+
+            println!("file to download: {}", file_name);
+
+            download_link(&wallpaper.data.url, file_name, "img").await?;
         }
         println!("{}", link_number);
     }
@@ -37,7 +55,7 @@ async fn fetch_from_reddit() -> Result<(), Box<dyn std::error::Error>> {
     println!("after = {}", value.data.after);
 
     Ok(())
-} 
+}
 
 fn main() {
     fetch_from_reddit();
